3f42a5121dd5bfc29793d13b74742ce9bebd1af6
Glenn <ghemhope@gmail.com>
2021-07-01 13:41:45 +0800

    add currency selectpr

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var btn = document.querySelector('#btn')
 var price = document.querySelector('#price')
+var currency = 'USD'
 
 
 
@@ -10,8 +11,9 @@ btn.addEventListener('click', function(){
     XHR.onreadystatechange = function(){
         if( XHR.readyState == 4 && XHR.status == 200){
                 var data = JSON.parse(XHR.responseText)
-                var rate = data.bpi.USD.rate
-                price.innerHTML = ` ${rate} ${data.bpi.USD.description}`
+                var rate = data.bpi[currency].rate
+                price.innerHTML = ` ${rate} ${currency}`
+                console.log(data.bpi)
         }
     } 
     XHR.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice.json')
