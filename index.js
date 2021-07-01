2dcd22e186d11e0bb28f5747bffe8627f9c1e33f
Glenn <ghemhope@gmail.com>
2021-07-01 13:30:54 +0800

    bpi

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert('sdfs')
+var btn = document.querySelector('#btn')
+var price = document.querySelector('#price')
+
+
+
+
+btn.addEventListener('click', function(){
+    var XHR = new XMLHttpRequest();
+
+    XHR.onreadystatechange = function(){
+        if( XHR.readyState == 4 && XHR.status == 200){
+                var data = JSON.parse(XHR.responseText)
+                var rate = data.bpi.USD.rate
+                price.innerHTML = ` ${rate} ${data.bpi.USD.description}`
+        }
+    } 
+    XHR.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice.json')
+    XHR.send()
+})
